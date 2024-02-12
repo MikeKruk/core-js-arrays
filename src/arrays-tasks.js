a9a03f7f0e82e32c2a98b788785345e5b5292e90
Mikhail Kruk <mi.kruk95@gmail.com>
2024-02-13 00:56:36 +0100

    feat: solve last task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -499,8 +499,24 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let max = 1;
+
+  const arrayWithSequences = nums.map((item, index, arr) => {
+    const nextItem = arr[index + 1];
+
+    if (nextItem > item) {
+      max += 1;
+    } else {
+      max = 1;
+    }
+
+    return max;
+  });
+
+  max = Math.max(...arrayWithSequences);
+
+  return max;
 }
 
 /**
